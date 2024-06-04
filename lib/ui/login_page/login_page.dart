1c91694e440a8231174479c67decd2fce9c75553
coderyu5523 <coderyu5523@naver.com>
2024-06-04 19:40:21 +0900

    naver login

--- lib/ui/login_page/login_page.dart
+++ lib/ui/login_page/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_login/flutter_naver_login.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:oauthapp/_core/http.dart';
 
@@ -11,11 +12,21 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("카카오 로그인"),
       ),
-      body: ElevatedButton(
-        child: Text("카카오로그인"),
-        onPressed: () async {
-          kakaoLogin();
-        },
+      body: Column(
+        children: [
+          ElevatedButton(
+            child: Text("카카오로그인"),
+            onPressed: () async {
+              kakaoLogin();
+            },
+          ),
+          ElevatedButton(
+            child: Text("네이버로그인"),
+            onPressed: () async {
+              NaverLogin();
+            },
+          ),
+        ],
       ),
     );
   }
@@ -28,7 +39,7 @@ class LoginPage extends StatelessWidget {
       print('카카오계정으로 로그인 성공 ${token.accessToken}');
 
       // 2. 토큰(카카오)을 스프링서버에 전달하기 (스프링 서버한테 나 인증했어!! 라고 알려주는것)
-      final response = await dio.get("/oauth/callback",
+      final response = await dio.get("/oauth/kakao/callback",
           queryParameters: {"accessToken": token.accessToken});
 
       // 3. 토큰(스프링서버) 응답받기
@@ -42,5 +53,29 @@ class LoginPage extends StatelessWidget {
     } catch (error) {
       print('카카오계정으로 로그인 실패 $error');
     }
+  }
+}
+
+void NaverLogin() async {
+  try {
+    // 1. 로그인 (토큰 가져오기)
+    await FlutterNaverLogin.logIn(); // 네이버 로그인 시도
+    NaverAccessToken token =
+        await FlutterNaverLogin.currentAccessToken; // 토큰 가져오기
+
+    print('네이버계정으로 로그인 성공 ${token.accessToken}');
+
+    //2. 토큰 서버로 보내기
+    final response = await dio.get("/oauth/naver/callback",
+        queryParameters: {"accessToken": token.accessToken});
+
+    // 3. 토큰 응답받기
+    final blogAccessToken = response.headers["Authorization"]!.first;
+    print("blogAccessToken : ${blogAccessToken}");
+
+    // 4. 시큐어 스토리지에 저장
+    secureStorage.write(key: "blogAccessToken", value: blogAccessToken);
+  } catch (error) {
+    print('네이버계정으로 로그인 실패 $error');
   }
 }
